215c38b139636a0e3e118d247a907ef18b027f64
Lienar <140402520+Lienar@users.noreply.github.com>
2024-09-15 15:21:00 +0300

    Add files via upload

---
+++ app/routers/task.py
@@ -0,0 +1,92 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from app.backend.db_depends import get_db
+from typing import Annotated
+
+from app.models import *
+from sqlalchemy import insert, update
+from app.schemas import CreateTask, UpdateTask
+
+from slugify import slugify
+from sqlalchemy import select
+
+router_task = APIRouter(prefix="/task", tags=["task"])
+
+
+@router_task.get("/")
+async def all_tasks(db: Annotated[Session, Depends(get_db)]):
+    tasks = db.scalars(select(Task)).all()
+    return tasks
+
+
+@router_task.get("/task_id")
+async def task_by_id(db: Annotated[Session, Depends(get_db)], task_id: int):
+    task = db.scalars((select(Task).where(Task.id == task_id))).one_or_none()
+    if task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='There is no task found'
+        )
+    return task
+
+
+@router_task.post("/create")
+async def create_task(db: Annotated[Session, Depends(get_db)], create_task: CreateTask, user_cr: str):
+    user = db.scalars((select(User).where(User.username == user_cr))).one_or_none()
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User was not found'
+        )
+
+    db.execute(insert(Task).values(title=create_task.title,
+                                   content=create_task.content,
+                                   priority=create_task.priority,
+                                   user_id=user.id,
+                                   slug=slugify(create_task.title)))
+    db.commit()
+    return {
+        'status_code': status.HTTP_201_CREATED,
+        'transaction': 'Successful'
+    }
+
+
+@router_task.put("/update")
+async def update_task(db: Annotated[Session, Depends(get_db)], task_id: int, update_task: UpdateTask):
+    task = db.scalars((select(Task).where(Task.id == task_id))).one_or_none()
+    if task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='There is no task found'
+        )
+
+    db.execute(update(Task).where(Task.id == task_id).values(
+        title=update_task.title,
+        content=update_task.content,
+        priority=update_task.priority,
+        completed=update_task.completed,
+        slug=slugify(update_task.title)))
+
+    db.commit()
+    return {
+        'status_code': status.HTTP_200_OK,
+        'transaction': 'Task update is successful'
+    }
+
+
+
+@router_task.delete("/delete")
+async def delete_task(db: Annotated[Session, Depends(get_db)], task_id: int):
+    task = db.scalars((select(Task).where(Task.id == task_id))).one_or_none()
+    if task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='There is no task found'
+        )
+
+    db.delete(task)
+    db.commit()
+    return {
+        'status_code': status.HTTP_200_OK,
+        'transaction': 'Task delete is successful'
+    }
